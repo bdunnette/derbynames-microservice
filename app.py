ee32cf87872ab25b9df59eb09f0f012eb01bc199
Brian Dunnette <dunn0172@umn.edu>
2022-08-31 15:33:54 -0500

    merging in dokku (#8)
    * use gunicorn
    
    * adding procfile
    
    * merging
    
    * ..or maybe flask > gunicorn?

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# import os
-
 from aitextgen import aitextgen
 from flask import Flask, jsonify, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+CORS(app)
 
 ai = aitextgen(model_folder="model", tokenizer_file="model/aitextgen.tokenizer.json")
 
@@ -15,6 +15,7 @@ def home(name=None):
 
 
 @app.route("/name/")
+# @cross_origin()
 def generate_name():
     generated = ai.generate_one().strip()
     app.logger.info(generated)
@@ -22,13 +23,8 @@ def generate_name():
 
 
 @app.route("/name/<prompt>")
+# @cross_origin()
 def generate_name_prompt(prompt):
     generated = ai.generate_one(prompt=prompt).strip()
     app.logger.info(generated)
     return jsonify({"name": generated, "prompt": prompt})
-
-
-# if __name__ == "__main__":
-# Bind to PORT if defined, otherwise default to 5000.
-# port = int(os.environ.get("PORT", 5000))
-# app.run(host="0.0.0.0", port=port)
